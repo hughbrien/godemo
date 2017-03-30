27cabb503610e509e1feb54a7612c10d111b76bd
Hugh Brien <hughbrien@Hughs-MacBook-Pro.local>
2017-03-30 15:27:28 -0400

    Added new files

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//result, result1 := fmt.Println("Hello World", 1, 2, 4,5,6, "more stuff")
+	fmt.Println("Hello World", 1, 2, 4,5,6, "more stuff")
+
+}
+
