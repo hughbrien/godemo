885756e5533166bace6b96957cf025819d517e9b
Hugh Brien <hughbrien@Hughs-MacBook-Pro.local>
2017-03-30 16:24:25 -0400

    Added Web components

--- main.go
+++ main.go
@@ -1,11 +1,93 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+	"fmt"
+	"time"
+	"github.com/instana/golang-sensor"
+	ot "github.com/opentracing/opentracing-go"
+)
+
+const (
+	Service = "goes-eleven-demo"
+	Entry   = "http://localhost:9060/golang/entry"
+	Exit    = "http://localhost:9060/golang/exit"
+)
+
+var demoOptions instana.Options
+
 
 func main() {
 
-	//result, result1 := fmt.Println("Hello World", 1, 2, 4,5,6, "more stuff")
-	fmt.Println("Hello World", 1, 2, 4,5,6, "more stuff")
+	fmt.Print ( " # # # # # # Starting CORE # # # # # # " )
+
+	opts := instana.Options {
+		Service: Service,
+		LogLevel: instana.Info,
+	}
+
+	ot.InitGlobalTracer(instana.NewTracerWithOptions(&opts))
+
+	//Span span  = ot.StartSpanFromContext(c)
+	fmt.Println ("Starting on port 8080")
+	myMux := http.NewServeMux()
+	myMux.HandleFunc("/", catchall)
+	myMux.HandleFunc("/service", demoservice)
+	myMux.HandleFunc("/products", products)
+	myMux.HandleFunc("/demos", demos)
+
+	http.ListenAndServe(":8080", myMux)
+}
+
+
+
+func catchall (w http.ResponseWriter, req *http.Request) {
+
+	demoOptions = instana.Options{"hostname","",0,0}
+
+	second := time.Second
+	miliseconds := int64(second/time.Millisecond)
+
+	fmt.Println ("\n")
+	var num int64 = int64(second/time.Millisecond)
+
+	fmt.Print(miliseconds)
+	numstring:=fmt.Sprintf("%d",num)
+
+	fmt.Print ( " # # # # # # # # # # # # " )
+	fmt.Println(numstring)
+	fmt.Print ( "Response Writer # # # # # # # # # # ")
+	fmt.Println(w)
+	fmt.Println("\n")
+	fmt.Println(" # # # # # # # # #  GoLang HTTP Request # # # # # # # # # # ")
+	fmt.Print(req)
+	fmt.Println("\n")
+	printPage(w)
 
 }
+
+
+
+
+func printPage( w http.ResponseWriter){
+
+	w.Write([]byte ("<h1>This is the first Text from the the Application Server</h1>"))
+
+}
+
+func demoservice (w http.ResponseWriter, req *http.Request) {
+
+	w.Write([]byte("Demo services takes 4 seconds "))
+	time.Sleep(time.Second * 4)
+	w.Write([]byte("\n"))
+}
+
+func products (w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("products"))
+}
+
+func demos (w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("demos"))
+}
+
 
